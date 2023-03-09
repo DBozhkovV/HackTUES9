4d2873f7d65f43b87a6d695db29c30af8c9415ef
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2023-03-09 12:01:32 +0200

    Upload User

---
+++ backend/BackEnd/Data/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id;
+
+        [Required]
+        public string Username;
+
+        [Required]
+        public string Email;
+
+        [Required]
+        public string Password;
+    }
+}
